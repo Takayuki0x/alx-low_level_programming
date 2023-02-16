3f7236e5aac133bda7298426212a9ca043e763e1
Anass Soummane <soummaneanass@gmail.com>
2023-02-16 16:20:04 +0100

    Created 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include  <stdio.h>
+
+/*
+ * main - Printing Dora Korpar's quote
+ *
+ * Description - This program prints Dora Korpar's quote
+ * without using printf or puts
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful \" - Dora Korpar, 2015-10-19", 59);
+	return (1);	
+}
