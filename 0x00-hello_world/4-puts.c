24020af6f5bd567e452bf88ff3b7ed01427dc3ac
Anass Soummane <soummaneanass@gmail.com>
2023-02-16 07:53:35 +0100

    Changed styling to be Betty-compliant

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void){
+/* Main function */
+
+int main(void)
+{
 	char st1[] = "\"Programming is like building a multilingual puzzle";
 	
 	puts(st1);
