737af00d2eacc57db05c1d102f33da07ae089aa7
Anass Soummane <soummaneanass@gmail.com>
2023-02-16 08:06:33 +0100

    Changed styling of 5-printf.c to be Betty-compliant

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
-int main(){
-	printf("with proper grammar, but the outcome is a piece of art, \n");
-	return 0;
+
+/**
+ * main - printing a string with printf
+ *
+ * Description - This program prints
+ * "with proper grammar, but the outcome is a piece of art,"
+ * using printf
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
