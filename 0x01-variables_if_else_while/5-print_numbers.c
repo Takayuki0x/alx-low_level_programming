adecf7fce2d016f4a4f90b7fafd7685aca8cd134
Anass Soummane <soummaneanass@gmail.com>
2023-02-17 22:54:40 +0100

    Created 5-printnumbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - prints all single digit numbers
+*
+* Description: using the puts function
+* this program prints all single digit numbers starting from 0
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+puts(i)
+}
+
+puts('\n');
+return (0);
+}
