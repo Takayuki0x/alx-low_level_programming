f34bfc9e38ea2aa1bfc349d70d54b2ef719e0e6a
Anass Soummane <soummaneanass@gmail.com>
2023-02-17 20:27:47 +0100

    Made 0 betty compliant

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,10 +4,10 @@
 
 /**
  * main - determines if n is positive or negative
- * 
+ *
  * Description - assigns a random number to variable n
  * prints if n is a positive, negative, or equal to 0
- * Return: 0 
+ * Return: 0
 */
 
 int main(void)
@@ -16,7 +16,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
     if (n > 0)
     {
         printf("%d is positive\n", n);
