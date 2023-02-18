8babd57757f08f1e8e5cc9ffa0f73d9885c0dfe1
Anass Soummane <soummaneanass@gmail.com>
2023-02-18 03:05:58 +0100

    initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - prints "_putchar"
+*
+* Description: using the putchar function
+* this program prints the string _putchar
+* Return: 0
+*/
+
+int main(void)
+{
+printf("_putchar");
+return (0);
+}
