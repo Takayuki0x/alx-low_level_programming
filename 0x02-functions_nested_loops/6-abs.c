8a2403ef17874906aa356bc47301aaa382a7c386
Anass Soummane <soummaneanass@gmail.com>
2023-02-21 22:48:17 +0100

    created abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of a number
+ * @n: number to use
+ *
+ * Return: 
+*/
+
+int _abs(int n)
+{
+if (n < 0)
+{
+return (n * -1);
+}
+else
+{
+return (n);
+}    
+}
