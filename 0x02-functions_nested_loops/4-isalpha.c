3a06def4157951a1861c133a26f76d99c651d044
Anass Soummane <soummaneanass@gmail.com>
2023-02-21 22:18:42 +0100

    Created isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - checks if character is alphabetic
+ * @c: character to check
+ *
+ * Return: 1 if c is a letter, returns 0 otherwise
+*/
+
+int _isalpha(int c)
+{
+if ((c > 96 && c < 123) || (c > 64 && c < 91))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
