da5df55cd84c52896142f458af711c1714d64f1a
Anass Soummane <soummaneanass@gmail.com>
2023-02-21 21:55:40 +0100

    Created print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - using 2 _putchar
+ * this program prints the entire alphabet in lowercase 10 times
+ *
+ * Return: 0
+*/
+
+void print_alphabet_x10(void)
+{
+	char i;
+    int d;
+
+    for (d = 0; d < 10; d++)
+    {
+        for (i = 'a'; i <= 'z'; i++)
+	    {
+		    _putchar(i);
+	    }
+    }
+    
+_putchar('\n');
+}
