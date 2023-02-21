519dfbcbbefe46b329203f2a641b47e30bed3a28
Anass Soummane <soummaneanass@gmail.com>
2023-02-21 23:28:22 +0100

    updated 24h

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,26 +8,26 @@
 
 void jack_bauer(void)
 {
-    int h = 0;
-	int m = 0;
-	int hr;
-	int mr;
+int h = 0;
+int m = 0;
+int hr;
+int mr;
 
-	while (h <= 23)
-	{
-	while (m <= 59)
-	{
-	mr = m % 10;
-	hr = h % 10;
-	_putchar(h / 10 + '0');
-	_putchar(hr + '0');
-	_putchar(':');
-	_putchar(m / 10 + '0');
-	_putchar(mr + '0');
-	m++;
-	_putchar('\n');
-	}
-	h++;
-	m = 0;
+while (h <= 23)
+{
+while (m <= 59)
+{
+mr = m % 10;
+hr = h % 10;
+_putchar(h / 10 + '0');
+_putchar(hr + '0');
+_putchar(':');
+_putchar(m / 10 + '0');
+_putchar(mr + '0');
+m++;
+_putchar('\n');
+}
+h++;
+m = 0;
 }
 }
