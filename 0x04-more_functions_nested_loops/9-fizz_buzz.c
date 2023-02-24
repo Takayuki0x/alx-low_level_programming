a72d65514651d5de1d7c24ad7b098ca91f86ab38
Anass Soummane <soummaneanass@gmail.com>
2023-02-24 01:52:21 +0100

    updated fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,17 +11,17 @@ int main(void)
 {
 int i;
 
-for (i = 1; i < 101; i++)
+for (i = 1; i <= 100; i++)
 {
-if (i % 5 == 0 && i % 3 == 0)
+if ((i % 5) == 0 && (i % 3) == 0)
 {
 printf("FizzBuzz ");
 }
-else if (i % 5 == 0)
+else if ((i % 5) == 0)
 {
 printf("Buzz ");
 }
-else if (i % 3 == 0)
+else if ((i % 3) == 0)
 {
 printf("Fizz ");
 }
@@ -29,6 +29,12 @@ else
 {
 printf("%d ", i);
 }
+
+/*if (i == 100)
+{
+continue;
+}*/
+
 }
 
 printf("\n");
