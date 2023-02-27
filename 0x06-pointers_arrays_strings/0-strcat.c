493f6515f1513f3ae8c7cfd3408619ddfc187ae5
Anass Soummane <soummaneanass@gmail.com>
2023-02-27 20:46:20 +0100

    Updated strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings and overwrites the null byte
+ * @src: string to contactenate
+ * @dest: string to contactenate to
+ * Return: Returns pointer to resulting string
+*/
+
+char *_strcat(char *dest, char *src)
+{
+
+int t, t2;
+
+t = 0;
+while (dest[t])
+		t++;
+for (t2 = 0; src[t2] ; t2++)
+dest[t++] = src[t2];
+
+return (dest);
+}
