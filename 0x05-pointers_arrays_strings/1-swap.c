6ef5a9e04c078a856553409d901fab333dc1a2ab
Anass Soummane <soummaneanass@gmail.com>
2023-02-27 05:28:07 +0100

    Created int swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two ints
+ * @a: int a to be swapped
+ * @b: int b to be swapped
+*/
+
+void swap_int(int *a, int *b)
+{
+int temp;
+temp = b;
+
+*b = a;
+*a = temp;
+}
